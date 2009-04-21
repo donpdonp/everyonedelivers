4d7ab2346f592815af4cbf166b69941a68feb15a
Don Park <don@donpark.org>
2009-04-21 10:52:42 -0700

    im not sure cloudkit is meant to be used inside rails.

--- config/environment.rb
+++ config/environment.rb
@@ -15,10 +15,6 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  config.gem "cloudkit"
   config.gem "authlogic"
 
   # config.gem "bj"
