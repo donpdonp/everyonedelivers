03c6d362564c72ddcde87c95ff404c52ea0ec89c
Don Park <don@donpark.org>
2009-04-20 17:20:45 -0700

    Deliveries

---
+++ spec/controllers/deliveries_controller_spec.rb
@@ -0,0 +1,10 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe DeliveriesController do
+
+  #Delete this example and add some real ones
+  it "should use DeliveriesController" do
+    controller.should be_an_instance_of(DeliveriesController)
+  end
+
+end
