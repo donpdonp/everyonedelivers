9cb33079faa32349f0abab6a8f0febf46b4cda9f
Don Park <don@donpark.org>
2009-04-20 17:21:00 -0700

    cloudkit

--- config/environment.rb
+++ config/environment.rb
@@ -18,8 +18,10 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "cloudkit"
+  config.gem "auth_logic"
 
+  # config.gem "bj"
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
