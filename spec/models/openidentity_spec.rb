dc33576b668d4b7968c66584144d1a322dcbbdb8
Don Park <don@donpark.org>
2009-04-24 13:37:22 -0700

    openid spec fix

--- spec/models/openidentity_spec.rb
+++ spec/models/openidentity_spec.rb
@@ -3,6 +3,8 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Openidentity do
   before(:each) do
     @valid_attributes = {
+      :url => "http://bob",
+      :user_id => 1
     }
   end
 
