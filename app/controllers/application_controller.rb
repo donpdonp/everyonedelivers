61cfb46c9bc2978f5128f757d8dfcbd379f181b9
Don Park <don@donpark.org>
2009-04-23 17:30:02 -0700

    authlogic straggler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,10 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-  helper_method :current_user_session, :current_user, :logged_in?
+  helper_method :current_user, :logged_in?
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 
-  include AuthlogicExtras
 end
