6cb98edcbe7bab4273a199bb559ad743ab50b047
Don Park <don@donpark.org>
2009-04-28 10:12:35 -0700

    typo in migration

--- db/migrate/20090428170519_create_journal.rb
+++ db/migrate/20090428170519_create_journal.rb
@@ -1,11 +1,11 @@
 class CreateJournal < ActiveRecord::Migration
   def self.up
     create_table :journal do |t|
-      t.integer delivery_id
-      t.integer package_id
-      t.integer user_id
-      t.integer location_id
-      t.float fee
+      t.integer :delivery_id
+      t.integer :package_id
+      t.integer :user_id
+      t.integer :location_id
+      t.float :fee
       t.timestamps
     end
   end
