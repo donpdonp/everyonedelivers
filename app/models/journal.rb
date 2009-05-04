e048c5877aadcfa36803eb785e5e024aefc75049
Don Park <don@donpark.org>
2009-05-04 13:08:21 -0700

    SETTINGS functionality

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -4,4 +4,9 @@ class Journal < ActiveRecord::Base
   belongs_to :package
   belongs_to :location
 
+  after_create :queue_an_email
+
+  def queue_an_email
+    EMAIL_QUEUE.publish({:to => SETTINGS["journal"]["notification_email_address"]}.to_yaml)
+  end
 end
--- config/initializers/bunny.rb
+++ config/initializers/bunny.rb
@@ -1,6 +1,7 @@
-BUNNY = Bunny.new
+BUNNY = Bunny.new(SETTINGS["bunny"]["start"])
 status = BUNNY.start
 if status == "CONNECTED"
+  EMAIL_QUEUE = BUNNY.queue('outbound_email')
   puts "RabbitMQ #{status}"
 else
   raise "RabbitMQ connection failed on #{BUNNY.host}:#{BUNNY.port}"
