2937bfb46e89542d94f22ecf81dd613b55b15e51
Don Park <don@donpark.org>
2009-05-05 08:08:42 -0700

    cover the users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,4 +7,11 @@ describe UsersController do
     controller.should be_an_instance_of(UsersController)
   end
 
+  it "should display details for a user" do
+    user = mock_model(User)
+    User.should_receive(:find_by_username).with(user.id.to_s).and_return(user)
+    Delivery.should_receive(:all).with({:conditions => {:listing_user_id => user.id}})
+    Delivery.should_receive(:all).with({:conditions => {:delivering_user_id => user.id}})
+    get :show, {:id => user.id}
+  end
 end
