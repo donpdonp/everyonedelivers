d9885c68711430240d4ac16f9422d82dda0808eb
Don Park <don@donpark.org>
2009-05-05 17:02:24 -0700

    edit the username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
+  before_filter :login_required, :only => [:edit, :update]
 
   def show
     @user = User.find_by_username(params[:id])
     @listed_deliveries = Delivery.all(:conditions => {:listing_user_id => @user.id})
     @accepted_deliveries = Delivery.all(:conditions => {:delivering_user_id => @user.id})
+  end
+
+  def edit
+    @user = User.find_by_username(params[:id])
+  end
+
+  def update
+    @user = User.find_by_username(params[:id])
+    if current_user == @user
+      flash[:notice] = "Profile updated"
+      @user.apply_form_attributes(params[:user])
+      @user.save!
+      redirect_to :action => :show, :id => @user.username
+    else
+      flash[:notice] = "Permission denied"
+      redirect_to root_path;
+    end    
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
   has_many :openidentities
 
+  def apply_form_attributes(params)
+    return if params.nil?
+
+    if params[:username]
+      self.username = params[:username]
+    end
+  end
+
   def openid
     self.openidentities.first
   end
