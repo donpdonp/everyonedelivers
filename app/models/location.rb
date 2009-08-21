1750a568f32095181dd80be4015393e34e46010f
Don Park <don@donpark.org>
2009-08-21 11:45:09 -0700

    javascript-heavy address validation in delivery creation.

--- app/models/location.rb
+++ app/models/location.rb
@@ -16,4 +16,7 @@ class Location < ActiveRecord::Base
     
   end
 
+  def gmap_static_html(h,w,z)
+   "http://maps.google.com/staticmap?center=#{self.latitude},#{self.longitude}&zoom=#{z}&size=#{h}x#{w}&key=#{SETTINGS["google"]["maps"]["key"]}&sensor=false&markers=#{self.latitude},#{self.longitude}"
+  end
 end
