bbf4004d43495bad2fb7006f215b9d6986bda196
Don Park <don@donpark.org>
2009-09-08 12:05:41 -0700

    move the listing title to the top of the item block

--- app/models/location.rb
+++ app/models/location.rb
@@ -23,4 +23,8 @@ class Location < ActiveRecord::Base
   def gmap_static_html(h,w,z)
    "http://maps.google.com/staticmap?center=#{self.latitude},#{self.longitude}&zoom=#{z}&size=#{h}x#{w}&key=#{SETTINGS["google"]["maps"]["key"]}&sensor=false&markers=#{self.latitude},#{self.longitude}"
   end
+
+  def street_parts
+    parts = street.split(',',street.count(','))
+  end
 end
