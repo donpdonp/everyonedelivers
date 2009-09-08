75b1d705fe00f1dcd52c011dd212ec1ffa2f8885
Don Park <don@donpark.org>
2009-09-08 11:22:36 -0700

    change the front page to one block of listings, up to a month old

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -8,8 +8,8 @@ describe DashboardController do
 
   it "should display the landing page" do
     Delivery.should_receive(:find_at_most_hours_old).and_return([])
-    Delivery.should_receive(:find_between_hours_old).and_return([])
-    Delivery.should_receive(:find_more_than_hours_old).and_return([])
+    #Delivery.should_receive(:find_between_hours_old).and_return([])
+    #Delivery.should_receive(:find_more_than_hours_old).and_return([])
     get :index
     response.should render_template("dashboard/index.html.erb")
   end
