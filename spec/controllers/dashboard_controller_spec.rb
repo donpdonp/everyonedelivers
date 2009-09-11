9cfb4b130ce36b6771551e6ae8971407889ea939
Don Park <don@donpark.org>
2009-09-11 11:16:16 -0700

    splash page spec fixes

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -6,10 +6,16 @@ describe DashboardController do
     controller.stub!(:set_timezone)
   end
 
-  it "should display the landing page" do
+  it "should display the splash page when logged out" do
+    get :index
+    response.should render_template("dashboard/splash.html.erb")
+  end
+
+  it "should display the dashboard when logged in" do
+    @user = mock_model(User)
+    controller.stub!(:current_user).and_return(@user)
+
     Delivery.should_receive(:find_at_most_hours_old).and_return([])
-    #Delivery.should_receive(:find_between_hours_old).and_return([])
-    #Delivery.should_receive(:find_more_than_hours_old).and_return([])
     get :index
     response.should render_template("dashboard/index.html.erb")
   end
