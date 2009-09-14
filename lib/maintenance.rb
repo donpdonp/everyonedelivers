a7fef585a06d9a630eb143293988d075bfe8f348
Don Park <don@donpark.org>
2009-09-14 10:37:55 -0700

    the user column in the journal, is it about the user or performed by the user?

--- lib/maintenance.rb
+++ lib/maintenance.rb
@@ -3,7 +3,7 @@ class Maintenance
     User.clocked_ins.each do |user|
       if user.clocked_in < Time.now - 1.hour
         user.clock_out!
-        Journal.create({:delivery => nil, :user => nil, :note => "Clocked out #{user.username} after an hour"})
+        Journal.create({:delivery => nil, :user => user, :note => "Clocked out #{user.username} after an hour"})
       end
     end
   end
