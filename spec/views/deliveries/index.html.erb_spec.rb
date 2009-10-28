cfbb4cf6c25f3acec6eefcd10ac08f062be98568
Don Park <don@donpark.org>
2009-10-28 16:29:26 -0700

    this spec was left out, a couple commits ago

---
+++ spec/views/deliveries/index.html.erb_spec.rb
@@ -0,0 +1,19 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe "/dashboard/index" do
+  before(:each) do
+  end
+
+  it "should render when someone is logged in" do
+    template.stub!(:logged_in?).and_return(true)
+    assigns[:delivery_groups] = []
+    assigns[:clocked_ins] = []
+    user = mock_model(User)
+    user.should_receive(:clocked_in?).and_return(false)
+    user.should_receive(:username).and_return("bob")
+    template.should_receive(:current_user).twice.and_return(user)
+    render 'deliveries/index'
+    response.should have_tag('p')
+  end
+
+end
