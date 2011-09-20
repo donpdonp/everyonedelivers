07de8419c8553f915c265f2080a628568a20d745
Don Park <don@donpark.org>
2011-09-20 11:25:19 -0700

    take out old login cruft

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   before_filter :set_timezone
 
-  # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
-  include LoginSystem
-
-  # alert someone on exceptions
-  include ExceptionNotifiable
-
   def set_timezone
     # current_user.time_zone #=> 'London'
     Time.zone = current_user.time_zone if current_user
