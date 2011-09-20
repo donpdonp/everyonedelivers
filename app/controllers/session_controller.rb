c891078e806009171eff93dff79a5f36709a9293
Don Park <don@donpark.org>
2011-09-20 15:46:29 -0700

    login token email

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,6 +4,15 @@ class SessionController < ApplicationController
     User.find_by_login_token(params[:login_token])
   end
 
+  def login
+    user = User.find_by_email(params[:email])
+    if user
+      UserMailer.welcome_email(user).deliver
+    else
+      User.create(:email => params[:email])
+    end
+  end
+
   def logout
     reset_session
     flash[:notice] = "logged out"
