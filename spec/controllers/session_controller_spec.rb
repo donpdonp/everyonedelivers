c2a44a4dddd8432773ead56d9034ee1770b9983e
Don Park <don@donpark.org>
2011-09-26 14:00:26 -0700

    login test

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -5,6 +5,14 @@ describe SessionController do
     controller.stub!(:set_timezone)
   end
 
+  it "logs in a first-time user" do
+    token = 'abc123'
+    user = mock_model(User)
+    User.should_receive(:find_by_authentication_token).with(token).and_return(user)
+    controller.should_receive(:sign_in)
+    get :login_token, {:token => token}
+  end
+
   it "should logout the user" do
     controller.should_receive(:reset_session)
     get :logout
