5895ab9b53f2a4600b05f141e4bf866daa7dd47b
Don Park <don@donpark.org>
2011-10-12 10:59:00 -0700

    map supports one mark or two markswith route

--- app/assets/javascripts/gmaps.js
+++ app/assets/javascripts/gmaps.js
@@ -1,31 +1,32 @@
   function mapinitialize(start_lat, start_long, end_lat, end_long, units) {
 
-    var start, end;
-    start = new google.maps.LatLng(start_lat, start_long);
-    end = new google.maps.LatLng(end_lat, end_long);
+    var start = new google.maps.LatLng(start_lat, start_long);
     var bounds = new google.maps.LatLngBounds();
     bounds.extend(start);
-    bounds.extend(end);
 
     var map = new google.maps.Map(document.getElementById("gmap"),
                                   {mapTypeId: google.maps.MapTypeId.ROADMAP,
                                    center: bounds.getCenter(),
                                    zoom: 12});
-    // this could be avoided if the zoom level could be calculated
-    map.fitBounds(bounds);
-
     var marker_start = new google.maps.Marker({position:start});
     marker_start.setMap(map);
-    var marker_end = new google.maps.Marker({position:end});
-    marker_end.setMap(map);
-     
 
-    var directions = new google.maps.DirectionsService();
-    var request = { origin : start,
-                    destination : end,
-                    travelMode : google.maps.DirectionsTravelMode.DRIVING,
-                    unitSystem: units };
-    directions.route(request, show_directions_callback_builder(map));
+    if (typeof(end_lat) == "number") {
+      var end = new google.maps.LatLng(end_lat, end_long);
+      bounds.extend(end);
+      map.fitBounds(bounds);
+
+      var marker_end = new google.maps.Marker({position:end});
+      marker_end.setMap(map);
+       
+
+      var directions = new google.maps.DirectionsService();
+      var request = { origin : start,
+                      destination : end,
+                      travelMode : google.maps.DirectionsTravelMode.DRIVING,
+                      unitSystem: units };
+      directions.route(request, show_directions_callback_builder(map));
+    }
   }
 
   function show_directions_callback_builder(map) {
