21fa4c9cfd717da94ef44a3f501c8e396b8e4ca2
Don Park <don@donpark.org>
2011-10-12 11:18:57 -0700

    support one or two mark maps

--- app/assets/javascripts/gmaps.js
+++ app/assets/javascripts/gmaps.js
@@ -1,32 +1,32 @@
-  function mapinitialize(start_lat, start_long, end_lat, end_long, units) {
-
+  function mapinitialize(start_lat, start_long) {
     var start = new google.maps.LatLng(start_lat, start_long);
-    var bounds = new google.maps.LatLngBounds();
-    bounds.extend(start);
-
     var map = new google.maps.Map(document.getElementById("gmap"),
                                   {mapTypeId: google.maps.MapTypeId.ROADMAP,
-                                   center: bounds.getCenter(),
+                                   center: start,
                                    zoom: 12});
     var marker_start = new google.maps.Marker({position:start});
     marker_start.setMap(map);
+    return map;
+  }
 
-    if (typeof(end_lat) == "number") {
-      var end = new google.maps.LatLng(end_lat, end_long);
-      bounds.extend(end);
-      map.fitBounds(bounds);
+  function map_destination(map, end_lat, end_long, units) {
+    var start = map.getCenter();
+    var end = new google.maps.LatLng(end_lat, end_long);
+    var bounds = new google.maps.LatLngBounds();
+    bounds.extend(start);
+    bounds.extend(end);
+    map.fitBounds(bounds);
 
-      var marker_end = new google.maps.Marker({position:end});
-      marker_end.setMap(map);
-       
+    var marker_end = new google.maps.Marker({position:end});
+    marker_end.setMap(map);
+     
 
-      var directions = new google.maps.DirectionsService();
-      var request = { origin : start,
-                      destination : end,
-                      travelMode : google.maps.DirectionsTravelMode.DRIVING,
-                      unitSystem: units };
-      directions.route(request, show_directions_callback_builder(map));
-    }
+    var directions = new google.maps.DirectionsService();
+    var request = { origin : start,
+                    destination : end,
+                    travelMode : google.maps.DirectionsTravelMode.DRIVING,
+                    unitSystem: units };
+    directions.route(request, show_directions_callback_builder(map));
   }
 
   function show_directions_callback_builder(map) {
