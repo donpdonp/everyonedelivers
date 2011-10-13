fddba67ea6d181c4afdbc85b0e1e579ee89d25c4
Don Park <don@donpark.org>
2011-10-13 11:33:46 -0700

    form validation

--- app/assets/javascripts/schedule_add.js
+++ app/assets/javascripts/schedule_add.js
@@ -1,4 +1,12 @@
-function schedule_submit() {
+function schedule_submit(event) {
   console.log("submit!");
-  
+  if($('#gmaplat').val().length > 0 &&
+     $('#gmaplng').val().length > 0 &&
+     $('#street1_field').val().length > 0 &&
+     $('#street2_field').val().length > 0 ) {
+    return true;
+  } else {
+    alert('Please position the map marker');
+    return false;  
+  }
 }
