9bf08e497ef0283b019a1069d391bea3d2e57681
Don Park <don@donpark.org>
2011-10-13 11:22:53 -0700

    record each street seperately

--- db/migrate/20111012174153_create_schedules.rb
+++ db/migrate/20111012174153_create_schedules.rb
@@ -5,7 +5,8 @@ class CreateSchedules < ActiveRecord::Migration
       t.datetime :ending_at
       t.float :latitude
       t.float :longitude
-      t.string :nearest_intersection
+      t.string :street1
+      t.string :street2
 
       t.integer :user_id
       t.timestamps
