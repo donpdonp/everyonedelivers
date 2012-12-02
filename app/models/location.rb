c16973729673511553b8b79aff2c6476924524ea
Don Park <don@donpark.org>
2012-12-02 22:37:31 +0000

    Bing emBetterment

--- app/models/location.rb
+++ app/models/location.rb
@@ -21,7 +21,9 @@ class Location < ActiveRecord::Base
   end
 
   def gmap_static_html(h,w,z)
-   "http://staticmap.openstreetmap.de/staticmap.php?center=#{self.latitude},#{self.longitude}&zoom=#{z}&size=#{h}x#{w}&markers=#{self.latitude},#{self.longitude}"
+  "http://dev.virtualearth.net/REST/v1/Imagery/Map/Road/?mapSize=#{h},#{w}&pushpin=#{self.latitude},#{self.longitude};33&key=#{SETTINGS["bing"]["maps"]["key"]}"
+#    "http://maps.google.com/staticmap?center=#{self.latitude},#{self.longitude}&zoom=#{z}&size=#{h}x#{w}&key=#{SETTINGS["google"]["maps"]["key"]}&sensor=false&markers=#{self.latitude},#{self.longitude}"
+#   "http://staticmap.openstreetmap.de/staticmap.php?center=#{self.latitude},#{self.longitude}&zoom=#{z}&size=#{h}x#{w}&markers=#{self.latitude},#{self.longitude}"
   end
 
   def street_parts
