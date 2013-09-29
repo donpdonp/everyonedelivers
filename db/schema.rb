426ca1838148c2ff6776d855d5df341aaf20a113
Don Park <don@donpark.org>
2013-09-29 21:57:35 +0000

    delayed job schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111012174153) do
+ActiveRecord::Schema.define(:version => 20130929215541) do
 
   create_table "comments", :force => true do |t|
     t.integer  "delivery_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20111012174153) do
     t.string   "locked_by"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "queue"
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
